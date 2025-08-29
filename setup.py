b8634443a1b04d2c055cbb74db57ee775d855efc
Claudiu <sichigea20@gmail.com>
2025-08-30 00:56:12 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="juridic_chatbot",
+    version="0.1.0",
+    author="Claudiu",
+    author_email="sichigea20@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
